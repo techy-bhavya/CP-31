54de8eaf7a0889deac5ab20e7aa831d061c732a6
Bhavya Choudhary <bhavyachoudhary1225@gmail.com>
2026-07-01 19:50:28 +0530

    800 Rated Problem 1

---
+++ Rating800Problem1.java
@@ -0,0 +1,37 @@
+import java.util.*;
+
+public class Rating800Problem1
+{
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int t;
+		t = sc.nextInt();
+		while(t-- > 0){
+		    int n = sc.nextInt();
+		    int k = sc.nextInt();
+		    int[] arr = new int[n];
+		    for(int i=0;i<arr.length;i++){
+		        arr[i] = sc.nextInt();
+		    }
+		    boolean sorted = true;
+		    for(int i=0;i<n-1;i++){
+		        if(arr[i]>arr[i+1]){
+		            sorted = false;
+		            break;
+		        }
+		    }
+		    if(k==1){
+		        if(sorted){
+		            System.out.println("YES");
+		        }
+		        else{
+		            System.out.println("NO");
+		        }
+		    }
+		    else{
+		        System.out.println("YES");
+		    }
+		    sc.close();
+		}
+	}
+}
